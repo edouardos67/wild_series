1ce98eb31cb98a6cc08b8d8849413939e116c782
Edouard <edouard.neveu67@gmail.com>
2019-11-24 10:52:59 +0100

    2nd commit quete 3

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -7,10 +7,10 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class WildController extends AbstractController
+{
     /**
      * @Route("/wild", name="wild_index")
      */
-{
     public function index() :Response
     {
         return $this->render('wild/index.html.twig',[
